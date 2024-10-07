2c647a1a35152beef5209efb5a7aa2a70e87662c
CherryChio <147364842+CherryChio@users.noreply.github.com>
2024-10-06 18:52:14 -0600

    Quitar la basura

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,3 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-
-module.exports = {
-  productionSourceMap: false,
-  chainWebpack: (config) => {
-    config.optimization.delete('splitChunks');
-  },
-};
-
