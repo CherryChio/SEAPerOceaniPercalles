f46dbcc0929998f4b489a668e3c0c3790a808607
CherryChio <147364842+CherryChio@users.noreply.github.com>
2024-10-06 18:22:13 -0600

    Disable Brotli compression in build

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  productionSourceMap: false,
+  chainWebpack: (config) => {
+    config.optimization.delete('splitChunks');
+  },
+};
+
